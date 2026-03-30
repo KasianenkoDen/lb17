910900fd3d4ec16d6bb7fee5e1300e7fa301c557
Fjhhd56 <snus363@gmail.com>
2026-03-30 19:32:34 +0300

    Добавьте файлы проекта.

---
+++ lb17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb17
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var rivnobedTriangle = new RivnobedTriangle { SizeOfStorona1 = 5, SizeOfStorona2 = 6, CytMijStoronami = 60};
+            var pramTriangle = new PramocutTriangle { SizeOfStorona1 = 5, SizeOfStorona2 = 6 };
+            var rivnTriangle = new RivnoStoronniTriangle {SizeOfStorona = 6};
+            printshape(rivnobedTriangle);
+            printshape(pramTriangle);
+            printshape(rivnTriangle);
+        }
+        static void printshape(Triangle triangle)
+        {
+            Console.WriteLine($"Perimitr: {triangle.Perimetr()} Area: {triangle.Area()}");
+        }
+    }
+}
---
+++ lb17/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb17
+{
+    internal abstract class Triangle
+    {
+        public abstract double Perimetr();
+        public abstract double Area();
+
+    }
+    class PramocutTriangle : Triangle
+    {
+        public double SizeOfStorona1 { get; set; }
+        public double SizeOfStorona2 { get; set; }
+
+
+        public override double Perimetr()
+        {
+            return SizeOfStorona1 + SizeOfStorona2 + Math.Pow((Math.Pow(SizeOfStorona1, 2) + Math.Pow(SizeOfStorona2, 2)), 0.5);
+        }
+
+        public override double Area()
+        {
+            return 0.5 * SizeOfStorona1 * SizeOfStorona2;
+        }
+    }
+    class RivnobedTriangle : Triangle
+    {
+        public double SizeOfStorona1 { get; set; }
+        public double SizeOfStorona2 { get; set; }
+        public double CytMijStoronami { get; set; }
+        
+        public override double Perimetr()
+        {
+            double radians = CytMijStoronami * Math.PI / 180;
+
+            double c = Math.Sqrt(Math.Pow(SizeOfStorona1, 2) + Math.Pow(SizeOfStorona2, 2) - 2 * SizeOfStorona1 * SizeOfStorona2 * Math.Cos(radians));
+            return SizeOfStorona1 + SizeOfStorona2 + c;
+        }
+        public override double Area()
+        {
+            double p = Perimetr()/2;
+            double c = Perimetr() - SizeOfStorona1 - SizeOfStorona1;
+            double h = (2 / SizeOfStorona1) * Math.Pow((p * (p - SizeOfStorona1)*(p - SizeOfStorona2)*(p-c)), 0.5);
+            return (1/2)*SizeOfStorona1*h;
+        }
+
+    }
+
+    class RivnoStoronniTriangle : Triangle
+    {
+        public double SizeOfStorona { get; set; }
+
+        public override double Perimetr()
+        {
+            return SizeOfStorona * 3;
+        }
+        public override double Area() 
+        {
+            return (Math.Pow(SizeOfStorona, 2) * Math.Pow(3, 0.5))/4;
+        }
+    }
+}
+
